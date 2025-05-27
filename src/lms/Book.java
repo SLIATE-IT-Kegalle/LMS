be5231ab8e98a50f2b14637deaafd86912589036
Dilhani Wepathana <ymrdilhani@sliate.ac.lk>
2025-05-27 11:16:28 +0530

    book complete

--- src/lms/Book.java
+++ src/lms/Book.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package lms;
 
 /**
@@ -10,5 +11,9 @@ package lms;
  * @author adminit
  */
 public class Book {
-    
+  
+    private static int bookID;
+    Book(int id){
+    bookID=id;
+    }
 }
